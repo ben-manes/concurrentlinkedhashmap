ef273a8fbb287f1fe340f1201c408d1e1d9774a0
Ben.Manes <Ben.Manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2008-10-01 06:54:48 +0000

    Restoring JDK5 compatability. We can remove this in v2, but Ehcache 1.6 targets JDK5. Initial performance tests show (1) JDK6 halves synchronized structures versus JDK5, (2) CLHM does not show as strong as gains as depicted on homepage. This probably warrents profiling, as a large number of changes were made since that data was taken.
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn/trunk@48 52670e78-85c4-11dd-9ffd-cdf95c2cae98

--- src/java/com/rc/util/concurrent/ConcurrentLinkedHashMap.java
+++ src/java/com/rc/util/concurrent/ConcurrentLinkedHashMap.java
@@ -220,9 +220,7 @@ public class ConcurrentLinkedHashMap<K, V> extends AbstractMap<K, V> implements 
                         return node;
                     }
                     State state = node.getState();
-                    if ((state == State.UNLINKED) || (state == State.UNLINKING)) {
-                        break; // retry poll as the node is being removed
-                    } else if (state == State.SENTINEL) {
+                    if (state == State.SENTINEL) {
                         return null;
                     }
                     continue; // retry CAS as the node is being linked by offer
@@ -676,6 +674,49 @@ public class ConcurrentLinkedHashMap<K, V> extends AbstractMap<K, V> implements 
             }
             ConcurrentLinkedHashMap.this.remove(current.getKey(), current.getValue());
             current = null;
+        }
+    }
+    
+    /**
+     * This duplicates {@link java.util.AbstractMap.SimpleEntry} until the class is made accessible.
+     */
+    private static final class SimpleEntry<K,V> implements Entry<K,V> {
+        private final K key;
+        private V value;
+
+        public SimpleEntry(K key, V value) {
+            this.key   = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            } else if (!(obj instanceof Entry)) {
+                return false;
+            }
+            Entry<?, ?> entry = (Entry<?, ?>) obj;
+            return eq(key, entry.getKey()) && eq(value, entry.getValue());
+        }
+        public int hashCode() {
+            return ((key   == null)   ? 0 :   key.hashCode()) ^
+                   ((value == null)   ? 0 : value.hashCode());
+        }
+        public String toString() {
+            return key + "=" + value;
+        }
+        private static boolean eq(Object o1, Object o2) {
+            return (o1 == null) ? (o2 == null) : o1.equals(o2);
         }
     }
 }
