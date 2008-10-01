3158905316243693978d13036856f88ffdb4de7a
Ben.Manes <Ben.Manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2008-10-01 01:01:51 +0000

    The test group "performance" now compares the concurrency characteristics of the different caching implementations.
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn@39 52670e78-85c4-11dd-9ffd-cdf95c2cae98

--- trunk/unittest/src/java/com/rc/util/concurrent/MultiThreadedTest.java
+++ trunk/unittest/src/java/com/rc/util/concurrent/MultiThreadedTest.java
@@ -1,13 +1,21 @@
 package com.rc.util.concurrent;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.rc.util.concurrent.caches.Cache;
-import com.rc.util.concurrent.performance.CachePerformanceTest;
 
 /**
  * The concurrent tests for the {@link ConcurrentLinkedHashMap}.
@@ -23,7 +31,7 @@ public final class MultiThreadedTest extends BaseTest {
     private Set<Integer> keys;
     private Set<Cache> types;
     
-    @BeforeClass(groups="performance")
+    @BeforeClass(alwaysRun=true)
     public void beforeMultiThreaded() {
         runs = Integer.valueOf(System.getProperty("performance.runs"));
         nThreads = Integer.valueOf(System.getProperty("performance.threads.num"));
@@ -44,10 +52,97 @@ public final class MultiThreadedTest extends BaseTest {
     @Test(groups="development")
     public void readWrite() throws InterruptedException {
         Set<Cache> types = EnumSet.of(Cache.CONCURRENT_FIFO, Cache.CONCURRENT_SECOND_CHANCE, Cache.CONCURRENT_LRU);
+        List<List<Integer>> sets = shuffle(nThreads, keys);
         for (Cache type : types) {
-            CachePerformanceTest concurrencyTest = new CachePerformanceTest(type, 20, true, 10000, 25, capacity);
-            concurrencyTest.executeLockTest();
-            validator.state((ConcurrentLinkedHashMap<Integer, Integer>) concurrencyTest.getCache());
+            ConcurrentLinkedHashMap<Integer, Integer> cache = type.create(capacity, capacity, nThreads);
+            execute(cache, new Thrasher<Integer>(cache, sets));
+            validator.state(cache);
+        }
+    }
+    
+    /**
+     * Forces contention on the cache, thereby determining its performance under various concurrency scenarios.
+     */
+    @Test(groups="performance")
+    public void concurrency() throws InterruptedException {
+        List<List<Integer>> sets = shuffle(nThreads, keys);
+        for (Cache type : types) {
+            long sum = 0;
+            info("Cache Type: %s:", type);
+            List<Long> times = new ArrayList<Long>(runs);
+            for (int i=0; i<runs; i++) {
+                Map<Integer, Integer> cache = type.create(capacity, capacity, nThreads);
+                long time = TimeUnit.NANOSECONDS.toMillis(execute(cache, new Thrasher<Integer>(cache, sets)));
+                info("#%d: %s ms", i, time);
+                times.add(time);
+                sum += time;
+            }
+            info("Average: %s ms", DecimalFormat.getInstance().format(sum/runs));
+            
+            // upper, lower 10% removed
+            sum = 0;
+            int bound = runs / 10;
+            Collections.sort(times);
+            for (int i=bound; i<(runs-bound); i++) {
+                sum += times.get(i);
+            }
+            info("Corrected Average: %s ms\n", DecimalFormat.getInstance().format(sum/(runs-2*bound)));
+        }
+    }
+    
+    /**
+     * @return The execution time of the test.
+     */
+    public long execute(Map<?, ?> cache, Runnable runner) throws InterruptedException {
+        long time = ConcurrentTestHarness.timeTasks(nThreads, runner);
+        cache.clear();
+        return time;
+    }
+    
+    /**
+     * Based on the passed in working set, creates N shuffled varients.
+     * 
+     * @param samples    The number of varients to create.
+     * @param workingSet The base working set to build from.
+     */
+    private <T> List<List<T>> shuffle(int samples, Collection<T> workingSet) {
+        List<List<T>> sets = new ArrayList<List<T>>(samples);
+        for (int i=0; i<samples; i++) {
+            List<T> set = new ArrayList<T>(workingSet);
+            Collections.shuffle(set);
+            sets.add(set);
+        }
+        return sets;
+    }
+    
+    /**
+     * Executes read or write operations against the cache, thereby testing the efficiency of the
+     * concurrency constructs guarding the cache. The worse the lock, the longer the execution time.
+     */
+    private final class Thrasher<T> implements Runnable {
+        private final Map<T, T> cache;
+        private final List<List<T>> sets;
+        private final AtomicInteger index;
+        
+        public Thrasher(Map<T, T> cache, List<List<T>> sets) {
+            this.index = new AtomicInteger();
+            this.cache = cache;
+            this.sets = sets;
+        }
+
+        public void run() {
+            int id = index.getAndIncrement();
+            Random random = new Random();
+            for (T key : sets.get(id)) {
+                if (percentWrite > random.nextInt(100)) {
+                    cache.put(key, key);
+                } else {
+                    cache.get(key);
+                }
+                if (contention) {
+                    Thread.yield();
+                }
+            }
         }
     }
 }
--- trunk/unittest/src/java/com/rc/util/concurrent/caches/Cache.java
+++ trunk/unittest/src/java/com/rc/util/concurrent/caches/Cache.java
@@ -15,78 +15,78 @@ import com.rc.util.concurrent.ConcurrentLinkedHashMap.EvictionPolicy;
 @SuppressWarnings("unchecked")
 public enum Cache {
     CONCURRENT_FIFO() { /** A concurrent linked hashmap, using FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.FIFO, capacity, nThreads);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.FIFO, capacity, nThreads);
         }
     },
     CONCURRENT_SECOND_CHANCE() { /** A concurrent linked hashmap, using second-chance FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.SECOND_CHANCE, capacity, nThreads);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.SECOND_CHANCE, capacity, nThreads);
         }
     },
     CONCURRENT_LRU() { /** A concurrent linked hashmap, using LRU eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.LRU, capacity, nThreads);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new ConcurrentLinkedHashMap<K, V>(EvictionPolicy.LRU, capacity, nThreads);
         }
     },
     FAST_FIFO() { /** ConcurrentMap and ConcurrentQueue, using FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new ConcurrentFifoMap<K, V>(capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new ConcurrentFifoMap<K, V>(capacity);
         }
     },
     FAST_FIFO_2C() { /** ConcurrentMap and ConcurrentQueue, using second-chance FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new SecondChanceMap<K, V>(capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new SecondChanceMap<K, V>(capacity);
         }
     },
-    LINKED_FAST_FIFO_2C() { /** ConcurrentMap and ConcurrentQueue and key-value links, using second-chance FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new SecondChanceLinkedMap<K, V>(capacity, nThreads);
+    FAST_FIFO_LINKED_2C() { /** ConcurrentMap and ConcurrentQueue and key-value links, using second-chance FIFO eviction */
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new SecondChanceLinkedMap<K, V>(capacity, nThreads);
         }
     },
     RW_FIFO() { /** LinkedHashMap in FIFO eviction, guarded by read/write lock */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new LockMap<K, V>(false, capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new LockMap<K, V>(false, capacity);
         }
     },
     LOCK_LRU() { /** LinkedHashMap in LRU eviction, guarded by lock */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new LockMap<K, V>(true, capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new LockMap<K, V>(true, capacity);
         }
     },
     SYNC_FIFO() { /** LinkedHashMap in FIFO eviction, guarded by synchronized monitor */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return Collections.synchronizedMap(new UnsafeMap<K, V>(false, capacity));
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) Collections.synchronizedMap(new UnsafeMap<K, V>(false, capacity));
         }
     },
     SYNC_LRU() { /** LinkedHashMap in LRU eviction, guarded by synchronized monitor */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return Collections.synchronizedMap(new UnsafeMap<K, V>(true, capacity));
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) Collections.synchronizedMap(new UnsafeMap<K, V>(true, capacity));
         }
     },
     UNBOUNDED() { /** ConcurrentMap with no eviction policy, initially sized to avoid internal resizing */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new ConcurrentHashMap<K, V>(max, 0.75f, 1000);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new ConcurrentHashMap<K, V>(max, 0.75f, 1000);
         }
     },
     EHCACHE_FIFO() { /** Ehcache, using FIFO eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new EhcacheMap<K, V>(false, capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new EhcacheMap<K, V>(false, capacity);
         }
     },
     EHCACHE_LRU() { /** Ehcache, using LRU eviction */
-        public <K, V> Map<K, V> create(int capacity, int max, int nThreads) {
-            return new EhcacheMap<K, V>(true, capacity);
+        public <K, V, T> T create(int capacity, int max, int nThreads) {
+            return (T) new EhcacheMap<K, V>(true, capacity);
         }
     };
     
     /**
      * Creates the local cache instance.
      *
-     * @param type     The cache type.
      * @param capacity The cache's capacity.
      * @param maxSize  The unbounded, max possible size.
+     * @param nThreads The max number of concurrency accesses.
      * @return         A cache wrapped under a {@link Map} interface. 
      */
-    public abstract <K, V> Map<K, V> create(int capacity, int max, int nThreads);
+    public abstract <K, V, T> T create(int capacity, int max, int nThreads);
 }
