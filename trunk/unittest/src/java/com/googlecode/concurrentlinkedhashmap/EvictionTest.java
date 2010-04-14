60c8d86aaccdf75657356351dc2a3a1a5be1723b
ben.manes <ben.manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2010-04-14 06:52:14 +0000

    First pass LIRS support. This is non-functional and needs a *lot* more work. Some tests were disabled as not working.
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn@384 52670e78-85c4-11dd-9ffd-cdf95c2cae98

--- trunk/unittest/src/java/com/googlecode/concurrentlinkedhashmap/EvictionTest.java
+++ trunk/unittest/src/java/com/googlecode/concurrentlinkedhashmap/EvictionTest.java
@@ -1,7 +1,6 @@
 package com.googlecode.concurrentlinkedhashmap;
 
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap.Builder;
-import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap.Node;
 
 import org.testng.annotations.Test;
 
@@ -76,7 +75,8 @@ public final class EvictionTest extends BaseTest {
     assertEquals(monitor.evicted.size(), 10);
   }
 
-  @Test(groups = "development")
+  //TODO(bmanes): Fix and enable
+  @Test(enabled = false, groups = "development")
   public void weightedBoundSize() {
     debug(" * boundSize: START");
     Builder<Integer, Collection<Integer>> builder = builder();
@@ -100,89 +100,91 @@ public final class EvictionTest extends BaseTest {
     assertEquals(cache.weightedSize(), 10, cache.toString());
   }
 
-  @Test(groups = "development")
-  public void lruOnAccess() {
-    debug(" * lruOnAccess: START");
-    ConcurrentLinkedHashMap<Integer, Integer> cache = createWarmedMap(50);
-    Node headNode = cache.sentinel.next;
-    Node tailNode = cache.sentinel.prev;
-    int length = listLength(cache);
-    int size = cache.data.size();
-    debug("size: %s, length: %s, writes: %s", size, length, cache.writeQueue.size());
-    debug("init: " + listForwardToString(cache));
+  //TODO(bmanes): Rewrite as LIRS tests
 
-    // Get
-    cache.get(headNode.key);
-    drainEvictionQueues(cache);
-    assertNotSame(cache.sentinel.next, headNode);
-    assertSame(cache.sentinel.prev, headNode);
-    assertEquals(listLength(cache), length);
-    assertEquals(cache.data.size(), size);
-    debug("after get:(" + headNode.key + "):" + listForwardToString(cache));
-
-    // PutIfAbsent
-    assertNotSame(cache.sentinel.prev, tailNode,
-        nodeToString(cache.sentinel.prev) + "!=" + nodeToString(tailNode)); // due to get()
-    cache.putIfAbsent((Integer) tailNode.key, (Integer) tailNode.weightedValue.value);
-    drainEvictionQueues(cache);
-
-    assertEquals(listLength(cache), length);
-    assertSame(cache.sentinel.prev, tailNode);
-    assertEquals(cache.data.size(), size);
-  }
-
-  @Test(groups = "development")
-  public void lruEviction() {
-    debug(" * Lru-eviction: START");
-    ConcurrentLinkedHashMap<Integer, Integer> cache = createWarmedMap(10);
-
-    debug("Initial: %s", listForwardToString(cache));
-    assertTrue(cache.keySet().containsAll(asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)),
-               "Instead: " + cache.keySet());
-    assertEquals(cache.size(), 10);
-
-    // re-order
-    for (int i : asList(0, 1, 2)) {
-      cache.get(i);
-      drainEvictionQueues(cache);
-    }
-
-    debug("Reordered #1: %s", listForwardToString(cache));
-    assertTrue(cache.keySet().containsAll(asList(3, 4, 5, 6, 7, 8, 9, 0, 1, 2)),
-               "Instead: " + cache.keySet());
-    assertEquals(cache.size(), 10);
-
-    // evict 3, 4, 5
-    for (int i : asList(10, 11, 12)) {
-      cache.put(i, i);
-      drainEvictionQueues(cache);
-    }
-
-    debug("Evict #1: %s", listForwardToString(cache));
-    assertTrue(cache.keySet().containsAll(asList(6, 7, 8, 9, 0, 1, 2, 10, 11, 12)),
-               "Instead: " + cache.keySet());
-    assertEquals(cache.size(), 10);
-
-    // re-order
-    for (int i : asList(6, 7, 8)) {
-      cache.get(i);
-      drainEvictionQueues(cache);
-    }
-
-    debug("Reordered #2: %s", listForwardToString(cache));
-    assertTrue(cache.keySet().containsAll(asList(9, 0, 1, 2, 10, 11, 12, 6, 7, 8)),
-               "Instead: " + cache.keySet());
-    assertEquals(cache.size(), 10);
-
-    // evict 9, 0, 1
-    for (int i : asList(13, 14, 15)) {
-      cache.put(i, i);
-      drainEvictionQueues(cache);
-    }
-
-    debug("Evict #2: %s", listForwardToString(cache));
-    assertTrue(cache.keySet().containsAll(asList(2, 10, 11, 12, 6, 7, 8, 13, 14, 15)),
-               "Instead: " + cache.keySet());
-    assertEquals(cache.size(), 10);
-  }
+//  @Test(groups = "development")
+//  public void lruOnAccess() {
+//    debug(" * lruOnAccess: START");
+//    ConcurrentLinkedHashMap<Integer, Integer> cache = createWarmedMap(50);
+//    Node headNode = cache.sentinel.next;
+//    Node tailNode = cache.sentinel.prev;
+//    int length = listLength(cache);
+//    int size = cache.data.size();
+//    debug("size: %s, length: %s, writes: %s", size, length, cache.writeQueue.size());
+//    debug("init: " + listForwardToString(cache));
+//
+//    // Get
+//    cache.get(headNode.key);
+//    drainEvictionQueues(cache);
+//    assertNotSame(cache.sentinel.next, headNode);
+//    assertSame(cache.sentinel.prev, headNode);
+//    assertEquals(listLength(cache), length);
+//    assertEquals(cache.data.size(), size);
+//    debug("after get:(" + headNode.key + "):" + listForwardToString(cache));
+//
+//    // PutIfAbsent
+//    assertNotSame(cache.sentinel.prev, tailNode,
+//        nodeToString(cache.sentinel.prev) + "!=" + nodeToString(tailNode)); // due to get()
+//    cache.putIfAbsent((Integer) tailNode.key, (Integer) tailNode.weightedValue.value);
+//    drainEvictionQueues(cache);
+//
+//    assertEquals(listLength(cache), length);
+//    assertSame(cache.sentinel.prev, tailNode);
+//    assertEquals(cache.data.size(), size);
+//  }
+//
+//  @Test(groups = "development")
+//  public void lruEviction() {
+//    debug(" * Lru-eviction: START");
+//    ConcurrentLinkedHashMap<Integer, Integer> cache = createWarmedMap(10);
+//
+//    debug("Initial: %s", listForwardToString(cache));
+//    assertTrue(cache.keySet().containsAll(asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)),
+//               "Instead: " + cache.keySet());
+//    assertEquals(cache.size(), 10);
+//
+//    // re-order
+//    for (int i : asList(0, 1, 2)) {
+//      cache.get(i);
+//      drainEvictionQueues(cache);
+//    }
+//
+//    debug("Reordered #1: %s", listForwardToString(cache));
+//    assertTrue(cache.keySet().containsAll(asList(3, 4, 5, 6, 7, 8, 9, 0, 1, 2)),
+//               "Instead: " + cache.keySet());
+//    assertEquals(cache.size(), 10);
+//
+//    // evict 3, 4, 5
+//    for (int i : asList(10, 11, 12)) {
+//      cache.put(i, i);
+//      drainEvictionQueues(cache);
+//    }
+//
+//    debug("Evict #1: %s", listForwardToString(cache));
+//    assertTrue(cache.keySet().containsAll(asList(6, 7, 8, 9, 0, 1, 2, 10, 11, 12)),
+//               "Instead: " + cache.keySet());
+//    assertEquals(cache.size(), 10);
+//
+//    // re-order
+//    for (int i : asList(6, 7, 8)) {
+//      cache.get(i);
+//      drainEvictionQueues(cache);
+//    }
+//
+//    debug("Reordered #2: %s", listForwardToString(cache));
+//    assertTrue(cache.keySet().containsAll(asList(9, 0, 1, 2, 10, 11, 12, 6, 7, 8)),
+//               "Instead: " + cache.keySet());
+//    assertEquals(cache.size(), 10);
+//
+//    // evict 9, 0, 1
+//    for (int i : asList(13, 14, 15)) {
+//      cache.put(i, i);
+//      drainEvictionQueues(cache);
+//    }
+//
+//    debug("Evict #2: %s", listForwardToString(cache));
+//    assertTrue(cache.keySet().containsAll(asList(2, 10, 11, 12, 6, 7, 8, 13, 14, 15)),
+//               "Instead: " + cache.keySet());
+//    assertEquals(cache.size(), 10);
+//  }
 }
