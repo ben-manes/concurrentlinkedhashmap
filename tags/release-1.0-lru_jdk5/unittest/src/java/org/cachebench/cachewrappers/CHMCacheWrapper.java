6ad1a1ece1cbe124c5a57d18642137568a147cbf
ben.manes <ben.manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2010-04-15 04:27:10 +0000

    Release v1.0-LRU_jdk5
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn@387 52670e78-85c4-11dd-9ffd-cdf95c2cae98

---
+++ tags/release-1.0-lru_jdk5/unittest/src/java/org/cachebench/cachewrappers/CHMCacheWrapper.java
@@ -0,0 +1,72 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package org.cachebench.cachewrappers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cachebench.CacheWrapper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author ben.manes@gmail.com (Ben Manes)
+ */
+public class CHMCacheWrapper implements CacheWrapper {
+  private final Log logger = LogFactory.getLog("org.cachebench.cachewrappers.LHMCacheWrapper");
+  private Map<Object, Object> cache;
+  private int capacity;
+
+  public void init(Map parameters) throws Exception {
+//    InputStream stream =
+//        getClass().getClassLoader().getResourceAsStream((String) parameters.get("config"));
+//    Properties props = new Properties();
+//
+//    props.load(stream);
+//    stream.close();
+//
+//    capacity = Integer.parseInt(props.getProperty("clhm.maximumCapacity"));
+
+    capacity = 5000;
+  }
+
+  public void setUp() throws Exception {
+    cache = new ConcurrentHashMap<Object, Object>(capacity);
+  }
+
+  public void tearDown() throws Exception {
+  }
+
+  public void put(List<String> path, Object key, Object value) throws Exception {
+    cache.put(key, value);
+  }
+
+  public Object get(List<String> path, Object key) throws Exception {
+    return cache.get(key);
+  }
+
+  public void empty() throws Exception {
+    cache.clear();
+  }
+
+  public int getNumMembers() {
+    return 0;
+  }
+
+  public String getInfo() {
+    return "size/capacity: " + cache.size() + "/" + capacity;
+  }
+
+  public Object getReplicatedData(List<String> path, String key) throws Exception {
+    return get(path, key);
+  }
+
+  public Object startTransaction() {
+    throw new UnsupportedOperationException("Does not support JTA!");
+  }
+
+  public void endTransaction(boolean successful) {
+    throw new UnsupportedOperationException("Does not support JTA!");
+  }
+}
