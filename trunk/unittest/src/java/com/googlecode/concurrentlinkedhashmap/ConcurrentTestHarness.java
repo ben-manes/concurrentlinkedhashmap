01ff034f32d52eac94c51c30a14862c952e432c9
ben.manes <ben.manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2010-04-23 04:30:50 +0000

    Minor cleaning up and added Charles' LIRS implementation which I am porting. I haven't debugged CLHM, so still broken.
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn@393 52670e78-85c4-11dd-9ffd-cdf95c2cae98

--- trunk/unittest/src/java/com/googlecode/concurrentlinkedhashmap/ConcurrentTestHarness.java
+++ trunk/unittest/src/java/com/googlecode/concurrentlinkedhashmap/ConcurrentTestHarness.java
@@ -9,152 +9,159 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 
 /**
  * A testing harness for concurrency related executions.
- *
- * This harness will ensure that all threads execute at the same instance, records
- * the full execution time, and optionally retrieves the responses from each thread.
- * This harness can be used for performance tests, investigations of lock contention,
- * etc.
- *
- * This code was adapted from <tt>Java Concurrency in Practice</tt>, using an example
- * of a {@link CountDownLatch} for starting and stopping threads in timing tests.
+ * <p/>
+ * This harness will ensure that all threads execute at the same time, records
+ * the full execution time, and optionally retrieves the responses from each
+ * thread. This harness can be used for performance tests, investigations of
+ * lock contention, etc.
+ * <p/>
+ * This code was adapted from <tt>Java Concurrency in Practice</tt>, using an
+ * example of a {@link CountDownLatch} for starting and stopping threads in
+ * timing tests.
  *
  * @author ben.manes@gmail.com (Ben Manes)
  */
 public final class ConcurrentTestHarness {
 
-    private ConcurrentTestHarness() {
-        throw new IllegalStateException("Cannot instantiate static class");
-    }
+  private ConcurrentTestHarness() {
+    throw new IllegalStateException("Cannot instantiate static class");
+  }
 
-    /**
-     * Executes a task, on N threads, all starting at the exact same instant.
-     *
-     * @param nThreads The number of threads to execute.
-     * @param task     The task to execute in each thread.
-     * @return         The execution time for all threads to complete, in nanoseconds.
-     */
-    public static long timeTasks(int nThreads, Runnable task) throws InterruptedException {
-        return timeTasks(nThreads, task, "Thread");
-    }
+  /**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @return the execution time for all threads to complete, in nanoseconds
+   */
+  public static long timeTasks(int nThreads, Runnable task) throws InterruptedException {
+    return timeTasks(nThreads, task, "Thread");
+  }
 
-    /**
-     * Executes a task, on N threads, all starting at the exact same instant.
-     *
-     * @param nThreads       The number of threads to execute.
-     * @param task           The task to execute in each thread.
-     * @param baseThreadName The base name for each thread in this task set.
-     * @return               The execution time for all threads to complete, in nanoseconds.
-     */
-    public static long timeTasks(int nThreads, Runnable task, String baseThreadName) throws InterruptedException {
-        return timeTasks(nThreads, Executors.callable(task), baseThreadName).getExecutionTime();
-    }
+  /**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @param baseThreadName the base name for each thread in this task set
+   * @return the execution time for all threads to complete, in nanoseconds
+   */
+  public static long timeTasks(int nThreads, Runnable task, String baseThreadName)
+      throws InterruptedException {
+    return timeTasks(nThreads, Executors.callable(task), baseThreadName).getExecutionTime();
+  }
 
-    /**
-     * Executes a task, on N threads, all starting at the exact same instant.
-     *
-     * @param nThreads The number of threads to execute.
-     * @param task     The task to execute in each thread.
-     * @return         The result of each task and the full execution time, in nanoseconds.
-     */
-    public static <T> TestResult<T> timeTasks(int nThreads, Callable<T> task) throws InterruptedException {
-        return timeTasks(nThreads, task, "Thread");
-    }
+  /**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @return the result of each task and the full execution time, in nanoseconds
+   */
+  public static <T> TestResult<T> timeTasks(int nThreads, Callable<T> task)
+      throws InterruptedException {
+    return timeTasks(nThreads, task, "Thread");
+  }
 
-    /**
-     * Executes a task, on N threads, all starting at the exact same instant.
-     *
-     * @param nThreads       The number of threads to execute.
-     * @param task           The task to execute in each thread.
-     * @param baseThreadName The base name for each thread in this task set.
-     * @return               The result of each task and the full execution time, in nanoseconds.
-     */
-    @SuppressWarnings("deprecation")
-    public static <T> TestResult<T> timeTasks(int nThreads, final Callable<T> task, final String baseThreadName) throws InterruptedException {
-        final CountDownLatch startGate = new CountDownLatch(1);
-        final CountDownLatch endGate = new CountDownLatch(nThreads);
-        final AtomicReferenceArray<T> results = new AtomicReferenceArray<T>(nThreads);
+  /**
+   * Executes a task, on N threads, all starting at the same time.
+   *
+   * @param nThreads the number of threads to execute
+   * @param task the task to execute in each thread
+   * @param baseThreadName the base name for each thread in this task set
+   * @return the result of each task and the full execution time, in
+   *     nanoseconds
+   */
+  @SuppressWarnings("deprecation")
+  public static <T> TestResult<T> timeTasks(int nThreads, final Callable<T> task,
+      final String baseThreadName)
+      throws InterruptedException {
+    final CountDownLatch startGate = new CountDownLatch(1);
+    final CountDownLatch endGate = new CountDownLatch(nThreads);
+    final AtomicReferenceArray<T> results = new AtomicReferenceArray<T>(nThreads);
 
-        List<Thread> threads = new ArrayList<Thread>(nThreads);
-        for (int i = 0; i < nThreads; i++) {
-            final int index = i;
-            Thread thread = new Thread(baseThreadName + "-" + i) {
-                @Override
-                public void run() {
-                    try {
-                        startGate.await();
-                        try {
-                            results.set(index, task.call());
-                        } finally {
-                            endGate.countDown();
-                        }
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-            thread.start();
-            threads.add(thread);
-        }
-
-        long start = System.nanoTime();
-        startGate.countDown();
-        try {
-            endGate.await();
-        } catch (InterruptedException e) {
-            for (Thread thread : threads) {
-                thread.stop();
+    List<Thread> threads = new ArrayList<Thread>(nThreads);
+    for (int i = 0; i < nThreads; i++) {
+      final int index = i;
+      Thread thread = new Thread(baseThreadName + "-" + i) {
+        @Override
+        public void run() {
+          try {
+            startGate.await();
+            try {
+              results.set(index, task.call());
+            } finally {
+              endGate.countDown();
             }
-            throw e;
+          } catch (Exception e) {
+            throw new RuntimeException(e);
+          }
         }
-        long end = System.nanoTime();
+      };
+      thread.start();
+      threads.add(thread);
+    }
 
-        return new TestResult<T>(end - start, toList(results));
+    long start = System.nanoTime();
+    startGate.countDown();
+    try {
+      endGate.await();
+    } catch (InterruptedException e) {
+      for (Thread thread : threads) {
+        thread.stop();
+      }
+      throw e;
+    }
+    long end = System.nanoTime();
+
+    return new TestResult<T>(end - start, toList(results));
+  }
+
+  /**
+   * Migrates the data from the atomic array to a {@link List} for easier
+   * consumption.
+   *
+   * @param data the per-thread results from the test
+   * @return the per-thread results as a standard collection
+   */
+  private static <T> List<T> toList(AtomicReferenceArray<T> data) {
+    List<T> list = new ArrayList<T>(data.length());
+    for (int i = 0; i < data.length(); i++) {
+      list.add(data.get(i));
+    }
+    return list;
+  }
+
+  /**
+   * The results of the test harness's execution.
+   *
+   * @param <T> the data type produced by the task
+   */
+  public static final class TestResult<T> {
+    private final long executionTime;
+    private final List<T> results;
+
+    public TestResult(long executionTime, List<T> results) {
+      this.executionTime = executionTime;
+      this.results = results;
     }
 
     /**
-     * Migrates the data from the atomic array to a {@link List} for easier consumption.
+     * The test's execution time, in nanoseconds.
      *
-     * @param data The per-thread results from the test.
-     * @return     The per-thread results as a standard collection.
+     * @return The time to complete the test.
      */
-    private static <T> List<T> toList(AtomicReferenceArray<T> data) {
-        List<T> list = new ArrayList<T>(data.length());
-        for (int i=0; i<data.length(); i++) {
-            list.add(data.get(i));
-        }
-        return list;
+    public long getExecutionTime() {
+      return executionTime;
     }
 
     /**
-     * The results of the test harness's execution.
+     * The results from executing the tasks.
      *
-     * @param <T> The data type produced by the task.
+     * @return The outputs from the tasks.
      */
-    public static final class TestResult<T> {
-        private final long executionTime;
-        private final List<T> results;
-
-        public TestResult(long executionTime, List<T> results) {
-            this.executionTime = executionTime;
-            this.results = results;
-        }
-
-        /**
-         * The test's execution time, in nanoseconds.
-         *
-         * @return The time to complete the test.
-         */
-        public long getExecutionTime() {
-            return executionTime;
-        }
-
-        /**
-         * The results from executing the tasks.
-         *
-         * @return The outputs from the tasks.
-         */
-        public List<T> getResults() {
-            return results;
-        }
+    public List<T> getResults() {
+      return results;
     }
+  }
 }
--- trunk/unittest/src/java/org/cachebench/cachewrappers/CHMCacheWrapper.java
+++ trunk/unittest/src/java/org/cachebench/cachewrappers/CHMCacheWrapper.java
@@ -1,83 +1,38 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-
 package org.cachebench.cachewrappers;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.cachebench.CacheWrapper;
+import com.googlecode.concurrentlinkedhashmap.caches.Cache;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * An implementation that delegates to a ConcurrentHashMap.
+ *
  * @author ben.manes@gmail.com (Ben Manes)
  */
-public class CHMCacheWrapper implements CacheWrapper {
-  private final Log logger = LogFactory.getLog("org.cachebench.cachewrappers.LHMCacheWrapper");
+public final class CHMCacheWrapper extends AbstractCacheWrapper {
   private Map<Object, Object> cache;
+  private int concurrencyLevel;
   private int capacity;
 
   @Override
   public void init(Map parameters) throws Exception {
-//    InputStream stream =
-//        getClass().getClassLoader().getResourceAsStream((String) parameters.get("config"));
-//    Properties props = new Properties();
-//
-//    props.load(stream);
-//    stream.close();
-//
-//    capacity = Integer.parseInt(props.getProperty("clhm.maximumCapacity"));
-
+    concurrencyLevel = 16;
     capacity = 5000;
   }
 
   @Override
   public void setUp() throws Exception {
-    cache = new ConcurrentHashMap<Object, Object>(capacity);
+    cache = Cache.CONCURRENT_HASH_MAP.create(capacity, concurrencyLevel);
   }
 
   @Override
-  public void tearDown() throws Exception {
+  protected int capacity() {
+    return Integer.MAX_VALUE;
   }
 
   @Override
-  public void put(List<String> path, Object key, Object value) throws Exception {
-    cache.put(key, value);
-  }
-
-  @Override
-  public Object get(List<String> path, Object key) throws Exception {
-    return cache.get(key);
-  }
-
-  @Override
-  public void empty() throws Exception {
-    cache.clear();
-  }
-
-  @Override
-  public int getNumMembers() {
-    return 0;
-  }
-
-  @Override
-  public String getInfo() {
-    return "size/capacity: " + cache.size() + "/" + capacity;
-  }
-
-  @Override
-  public Object getReplicatedData(List<String> path, String key) throws Exception {
-    return get(path, key);
-  }
-
-  @Override
-  public Object startTransaction() {
-    throw new UnsupportedOperationException("Does not support JTA!");
-  }
-
-  @Override
-  public void endTransaction(boolean successful) {
-    throw new UnsupportedOperationException("Does not support JTA!");
+  protected Map<Object, Object> delegate() {
+    return cache;
   }
 }
