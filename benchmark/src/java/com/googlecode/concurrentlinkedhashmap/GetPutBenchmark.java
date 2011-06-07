27d2c17ccf20c2ab62fdd0f5ab0dbf07e7fadb5c
ben.manes <ben.manes@52670e78-85c4-11dd-9ffd-cdf95c2cae98>
2011-06-07 03:29:18 +0000

    Fix copyright (assign to Google).
    git-svn-id: https://concurrentlinkedhashmap.googlecode.com/svn/branches/lru@677 52670e78-85c4-11dd-9ffd-cdf95c2cae98

--- benchmark/src/java/com/googlecode/concurrentlinkedhashmap/GetPutBenchmark.java
+++ benchmark/src/java/com/googlecode/concurrentlinkedhashmap/GetPutBenchmark.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.concurrentlinkedhashmap;
-
 import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.caliper.Param;
@@ -9,6 +23,7 @@ import com.googlecode.concurrentlinkedhashmap.caches.Cache;
 import com.googlecode.concurrentlinkedhashmap.caches.CacheBuilder;
 
 import java.util.Map;
+
 
 /**
  * A benchmark comparing the read/write performance at different ratios.
